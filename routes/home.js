aecbc867066ee340f1c0f1446876e997aaaf6b93
sanchezmigben <82328456+sanchezmigben@users.noreply.github.com>
2023-02-03 18:49:47 +0100

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
+  console.log("Ha entrado en la ruta home");
   return res.status(200).json({
     title: "Express Testing",
     message: "The app is working properly!",
