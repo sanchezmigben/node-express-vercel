6bf16469a3ae1a704c15e7c2f8d335e6ca45e5e1
migben <ma.sanchezbenito@edu.gva.es>
2023-02-07 19:04:57 +0100

    prueba

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  console.log("Ha entrado en la ruta home");
+  console.log("Ha entrado en la ruta homeee");
   return res.status(200).json({
-    title: "Express Testinggggggg",
+    title: "Express Testinggggggggg",
     message: "The app is working properly!",
   });
 });
