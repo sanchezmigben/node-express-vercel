3c58efdf0ba2030f121f6a2520d9dcc032da6568
migben <ma.sanchezbenito@edu.gva.es>
2023-02-08 16:49:53 +0100

    mongodb

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
+const config = require("./config.json")
+const api_key_atlas = config.api_atlas_key
+const connectionStringURLAtlasAPI = config.connectionStringURLAtlasAPI
+
+const mongoose = require("./utils/mongodb.config")
+const axios = require('axios'); //ATLAS API REST
+
+//console.log(config)
+//console.log(config.api_atlas_data)
 
 // Middlewares
 const app = express();
 app.use(express.json());
+
 
 app.get("/",(req,res)=>{
   res.send("Ruta raizzz")
@@ -13,6 +23,77 @@ app.get("/",(req,res)=>{
 // Routes
 app.use("/home", home);
 
+
+/******/
+app.get('/services', (req, res) => {
+
+  /*const { category } = req.query;
+
+  var data;
+  if (category) { 
+      data = JSON.stringify({
+          "collection": "products",
+          "database": "farmStand",
+          "dataSource": "Cluster0",
+          "filter":{
+              "category":category
+          }
+      });
+  } else {
+      data = JSON.stringify(config.api_atlas_data);
+  }*/
+
+  //console.log(config.api_atlas_data)
+  //data = JSON.stringify(config.api_atlas_data);
+  data = JSON.stringify({
+          /*"collection": config.api_atlas_collection,
+          "database": config.api_atlas_database,
+          "dataSource": config.api_atlas_dataSource*/
+          "collection": "services",
+          "database": "buildingsDB",
+          "dataSource": "Cluster0",
+          /*"projection":{
+            "_id":1
+          }*/
+
+  });
+  console.log(data)
+
+  var config = {
+      method: 'post',
+      url: connectionStringURLAtlasAPI + '/action/find',
+      headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Request-Headers': '*',
+          'api-key': api_key_atlas
+      },
+      data : data
+  };
+
+  
+  axios(config)
+  .then(function (response) {
+      const services = response.data;
+            
+      res.json(services);
+      /*if (category) {           
+          res.render('products/index', { products, category })
+      } else {            
+          res.render('products/index', { products, category: 'All' })
+      }*/
+  })
+  .catch(function (error) {
+      console.log(error);
+  });
+  
+})
+/******/
+
+
 // connection
-const port = process.env.PORT || 9001;
-app.listen(port, () => console.log(`Listening to port ${port}`));
+const port = process.env.PORT || config.api_port;
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`)
+  //Después de levantar el servidor, conectar con la BD Mongo
+  mongoose.establishConexion()    
+});
---
+++ utils/mongodb.config.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+const config = require("../config.json");
+//const logger = require("../utils/logger");
+const connectionOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+
+//const dbConnMongoDB = ""
+mongoose.set("strictQuery", true)
+
+mongoose.establishConexion = function(){
+    let connectionString = null
+    let msg = ""
+    switch(config.DBConnectionType){
+        case "Atlas":
+            connectionString = config.connectionStringAtlas
+            msg = "MONGO CONNECTION OPEN WITH ATLAS!!!"
+            break;
+        case "Local":
+            connectionString = config.connectionStringLocal
+            msg = "MONGO CONNECTION OPEN WITH LOCALHOST!!!"
+            break;
+        default:
+            connectionString = null
+    }
+    if(connectionString == null){
+        console.log("USING MONGO DB ATLAS API CONNECTION!!!")
+    } else {
+        mongoose.connect(connectionString, connectionOptions)
+            .then(() => {
+                console.log(msg)
+                //logger.access.info(msg)
+            })
+            .catch(err => {
+                console.log("OH NO MONGO CONNECTION ERROR!!!! Desc: " + err)
+                //logger.error.fatal("OH NO MONGO CONNECTION ERROR!!!! Desc: " + err)
+                //console.log(err)
+            })
+    }
+}
+
+module.exports = mongoose
