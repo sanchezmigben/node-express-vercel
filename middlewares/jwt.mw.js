2dac3308d3aced80e15eb5801a707851dd5a78c8
migben <ma.sanchezbenito@edu.gva.es>
2023-02-20 17:54:18 +0100

    jwt

--- middlewares/jwt.mw.js
+++ middlewares/jwt.mw.js
@@ -1,8 +1,24 @@
 
 module.exports = requireLogin
 
+
+function extractToken (req) {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        return req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+        return req.query.token;
+    } else if (req.session && req.session.token){
+        return req.session.token
+    }
+    return null;
+  }
+
 function requireLogin(req,res,next){
-    if(req.session && req.session.userLogued){
+    const token = extractToken(req)
+
+    console.log("User Logued Session: " + req.session.userLogued)
+
+    if((req.session && req.session.userLogued) || token){
         next()
     }else{
         res.status(401).json({msg:"No estás autorizado", code:401})
